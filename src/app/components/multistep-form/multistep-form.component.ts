c8600eea72c4465233a217da501fd52bf6b70c6c
Gw3i <contact.johngoldco@gmail.com>
2022-12-03 22:49:05 +0100

    Add stepper and position image

--- src/app/components/multistep-form/multistep-form.component.ts
+++ src/app/components/multistep-form/multistep-form.component.ts
@@ -1,15 +1,15 @@
 import {
   ChangeDetectionStrategy,
   Component,
-
   ViewEncapsulation,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StepperComponent } from '../stepper/stepper.component';
 
 @Component({
   selector: 'app-multistep-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, StepperComponent],
   templateUrl: './multistep-form.component.html',
   styleUrls: ['./multistep-form.component.scss'],
   encapsulation: ViewEncapsulation.None,
---
+++ src/app/components/stepper/stepper.component.ts
@@ -0,0 +1,24 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-stepper',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './stepper.component.html',
+  styleUrls: ['./stepper.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class StepperComponent {
+  @Input()
+  currentStep: number | null = null;
+
+  steps: number[] = [1, 2, 3, 4];
+}

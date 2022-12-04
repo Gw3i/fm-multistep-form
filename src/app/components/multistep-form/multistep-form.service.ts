86c88f35e3bad318e637ca4b9c67cd014b88c50a
Gw3i <contact.johngoldco@gmail.com>
2022-12-04 22:56:55 +0100

    Add 2 step styling for desktop

--- src/app/components/multistep-form/multistep-form.service.ts
+++ src/app/components/multistep-form/multistep-form.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class MultistepFormService {
-  currentStep: number | null = 1;
+  currentStep: number | null = 2;
 
   steps: number[] = [1, 2, 3, 4];
 }

7e6388243a7b5e35b0d1725687f65e5faae5d69a
Gw3i <contact.johngoldco@gmail.com>
2022-12-05 22:59:37 +0100

    Adjust styling for xk viewport for 3rd form step

--- src/app/components/multistep-form/multistep-form.service.ts
+++ src/app/components/multistep-form/multistep-form.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class MultistepFormService {
-  currentStep: number | null = 2;
+  currentStep: number | null = 3;
 
   steps: number[] = [1, 2, 3, 4];
 }

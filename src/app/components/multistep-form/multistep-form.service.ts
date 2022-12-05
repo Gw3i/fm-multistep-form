07770bec98431fb0909edaf8801e47384d98f9ae
Gw3i <contact.johngoldco@gmail.com>
2022-12-05 23:28:10 +0100

    Adjust styling for xl viewport for last form step

--- src/app/components/multistep-form/multistep-form.service.ts
+++ src/app/components/multistep-form/multistep-form.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class MultistepFormService {
-  currentStep: number | null = 3;
+  currentStep: number | null = 5;
 
   steps: number[] = [1, 2, 3, 4];
 }

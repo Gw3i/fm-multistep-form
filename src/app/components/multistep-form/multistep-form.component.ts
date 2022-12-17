97ff34cf989874b46661e77bcab58099e8e132f8
Gw3i <contact.johngoldco@gmail.com>
2022-12-17 10:36:36 +0100

    Style input

--- src/app/components/multistep-form/multistep-form.component.ts
+++ src/app/components/multistep-form/multistep-form.component.ts
@@ -13,11 +13,12 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, StepperComponent, ReactiveFormsModule],
   templateUrl: './multistep-form.component.html',
+  styleUrls: ['./multistep-form.component.scss'],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MultistepFormComponent implements OnInit {
-  currentStep = 1;
+  currentStep = 2;
   lastPage = false;
   form!: FormGroup;
 

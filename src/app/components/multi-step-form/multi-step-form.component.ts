a6cf2e29f3aaa77e2e3df36ecc8e45e8cc80ad99
Gw3i <contact.johngoldco@gmail.com>
2022-12-23 21:44:20 +0100

    Add validation

--- src/app/components/multi-step-form/multi-step-form.component.ts
+++ src/app/components/multi-step-form/multi-step-form.component.ts
@@ -5,7 +5,12 @@ import {
   OnInit,
   ViewEncapsulation,
 } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { StepperComponent } from '../stepper/stepper.component';
 
 @Component({
@@ -18,7 +23,7 @@ import { StepperComponent } from '../stepper/stepper.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MultiStepFormComponent implements OnInit {
-  currentStep = 2;
+  currentStep = 1;
   lastPage = false;
   form!: FormGroup;
   billingPeriod: 'monthly' | 'yearly' = 'monthly';
@@ -32,9 +37,16 @@ export class MultiStepFormComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      name: new FormControl(null),
-      email: new FormControl(null),
-      phone: new FormControl(null),
+      name: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.pattern("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$"),
+      ]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      phone: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(20),
+      ]),
 
       plan: new FormControl('arcadePlan'),
 
